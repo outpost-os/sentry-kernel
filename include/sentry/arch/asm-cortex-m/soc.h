7bd0447b2437dd9b0382068f408f0a2abaae496e
pthierry <philippe.thierry@ledger.fr>
2023-09-19 10:14:44 +0200

    adding clean header listing
    using generated headers instead of hardcoded ones

--- include/sentry/arch/asm-cortex-m/soc.h
+++ include/sentry/arch/asm-cortex-m/soc.h
@@ -4,20 +4,7 @@
 #ifndef SOC_H
 #define SOC_H
 
-#if defined(CONFIG_ARCH_MCU_STM32F407)
-#include <sentry/arch/asm-cortex-m/stm32f407/soc.h>
-#elif defined(CONFIG_ARCH_MCU_STM32F419)
-#include <sentry/arch/asm-cortex-m/stm32f419/soc.h>
-#elif defined(CONFIG_ARCH_MCU_STM32F429)
-#include <sentry/arch/asm-cortex-m/stm32f429/soc.h>
-#elif defined(CONFIG_ARCH_MCU_STM32F439)
-#include <sentry/arch/asm-cortex-m/stm32f439/soc.h>
-#elif defined(CONFIG_ARCH_MCU_STM32WB55)
-#include <sentry/arch/asm-cortex-m/stm32wb55/soc.h>
-#elif defined(CONFIG_ARCH_MCU_STM32U5)
-#include <sentry/arch/asm-cortex-m/stm32u5/soc.h>
-#else
-#error "unsupported SoC"
-#endif
+#include <sentry/arch/asm-cortex-m/irq_defs.h>
+#include <sentry/arch/asm-cortex-m/layout.h>
 
 #endif/*!SOC_H*/
