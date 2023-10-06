869297d420e65f7417fa607d6f316a979aaa3462
pthierry <philippe.thierry@ledger.fr>
2023-10-06 15:57:32 +0200

    framac: enhance entropy source to fully defined framaC api

--- proof/proof_bsp/framac_tools.h
+++ proof/proof_bsp/framac_tools.h
@@ -9,25 +9,70 @@ extern volatile uint8_t Frama_C_entropy_source_8 __attribute__((unused)) __attri
 extern volatile uint16_t Frama_C_entropy_source_16 __attribute__((unused)) __attribute__((FRAMA_C_MODEL));
 extern volatile uint32_t Frama_C_entropy_source_32 __attribute__((unused)) __attribute__((FRAMA_C_MODEL));
 
+//@ assigns Frama_C_entropy_source_8 \from Frama_C_entropy_source_8;
+static inline void Frama_C_update_entropy_8(void) {
+  Frama_C_entropy_source_8 = Frama_C_entropy_source_8;
+}
+
+//@ assigns Frama_C_entropy_source_16 \from Frama_C_entropy_source_16;
+static inline void Frama_C_update_entropy_16(void) {
+  Frama_C_entropy_source_16 = Frama_C_entropy_source_16;
+}
+
+//@ assigns Frama_C_entropy_source_32 \from Frama_C_entropy_source_32;
+static inline void Frama_C_update_entropy_32(void) {
+  Frama_C_entropy_source_32 = Frama_C_entropy_source_32;
+}
+
 /*@ requires order: 0 <= min <= max <= 255;
     assigns \result \from min, max, Frama_C_entropy_source_8;
     assigns Frama_C_entropy_source_8 \from Frama_C_entropy_source_8;
     ensures result_bounded: min <= \result <= max ;
  */
-uint8_t Frama_C_interval_8(uint8_t min, uint8_t max);
+static inline uint8_t Frama_C_interval_8(uint8_t min, uint8_t max)
+{
+  uint8_t r,aux;
+  Frama_C_update_entropy_8();
+  aux = Frama_C_entropy_source_8;
+  if ((aux>=min) && (aux <=max))
+    r = aux;
+  else
+    r = min;
+  return r;
+}
 
 /*@ requires order: 0 <= min <= max <= 65535;
     assigns \result \from min, max, Frama_C_entropy_source_16;
     assigns Frama_C_entropy_source_16 \from Frama_C_entropy_source_16;
     ensures result_bounded: min <= \result <= max ;
  */
-uint16_t Frama_C_interval_16(uint16_t min, uint16_t max);
+static inline uint16_t Frama_C_interval_16(uint16_t min, uint16_t max)
+{
+  uint16_t r,aux;
+  Frama_C_update_entropy_16();
+  aux = Frama_C_entropy_source_16;
+  if ((aux>=min) && (aux <=max))
+    r = aux;
+  else
+    r = min;
+  return r;
+}
 
 /*@ requires order: 0 <= min <= max <= 4294967295;
     assigns \result \from min, max, Frama_C_entropy_source_32;
     assigns Frama_C_entropy_source_32 \from Frama_C_entropy_source_32;
     ensures result_bounded: min <= \result <= max ;
  */
-uint32_t Frama_C_interval_32(uint32_t min, uint32_t max);
+static inline uint32_t Frama_C_interval_32(uint32_t min, uint32_t max)
+{
+  uint32_t r,aux;
+  Frama_C_update_entropy_32();
+  aux = Frama_C_entropy_source_32;
+  if ((aux>=min) && (aux <=max))
+    r = aux;
+  else
+    r = min;
+  return r;
+}
 
 #endif/*FRAMAC_TOOLS_H*/
