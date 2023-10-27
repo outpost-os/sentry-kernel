c75d1b526666cbef47c016a047ca01347a5cf3f0
yhql <victor_servant@hotmail.com>
2023-10-27 17:01:51 +0200

    fix: clippy warning

--- uapi/src/uapi.rs
+++ uapi/src/uapi.rs
@@ -3,9 +3,9 @@ use systypes::ResourceHandle;
 
 bitflags! {
     pub struct ResourceHandleBits: u32 {
-        const RESOURCE_TYPE = 0b000000000000_000000000000_1111;
-        const TASK_NS       = 0b000000000000_111111111111_0000;
-        const RESOURCE_ID   = 0b111111111111_000000000000_0000;
+        const RESOURCE_TYPE = 0b0000_0000_0000_0000_0000_0000_1111;
+        const TASK_NS       = 0b0000_0000_0000_1111_1111_1111_0000;
+        const RESOURCE_ID   = 0b1111_1111_1111_0000_0000_0000_0000;
     }
 }
 
