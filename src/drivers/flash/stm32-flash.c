f486ca0f69b786b3c558270bf088e3800f86fba0
pthierry <philippe.thierry@ledger.fr>
2023-11-08 15:24:04 +0100

    adding mm map to flash

--- src/drivers/flash/stm32-flash.c
+++ src/drivers/flash/stm32-flash.c
@@ -9,25 +9,37 @@
 #include <sentry/io.h>
 #include <sentry/bits.h>
 #include <sentry/ktypes.h>
+#include <sentry/managers/memory.h>
 #include <bsp/drivers/flash/flash.h>
 
 #include "flash_defs.h"
+#include "stm32-flash-dt.h"
 
-{%- set flash_controller = dts.get_compatible("st,stm32-flash-controller") %}
-
-{#- XXX, This driver handle one, and only one, flash controller #}
-{%- if flash_controller is defined and flash_controller[0].status == "okay" %}
-
-#define STM32F4_FLASH_W8_STATE {{ flash_controller[0]["wait-state"] }}UL
 #define STM32F4_FLASH_W8_STATE_TIMEOUT 0x100UL
+
+static inline kstatus_t flash_map(void)
+{
+    stm32_fmc_desc_t const * desc = stm32_fmc_get_desc(0);
+    return mgr_mm_map_kdev(desc->base_addr, desc->size);
+}
+/* for simplicity sake, but unmaping a kernel device is generic */
+static inline kstatus_t flash_unmap(void) {
+    return mgr_mm_unmap_kdev();
+}
 
 kstatus_t flash_probe(void)
 {
+
     uint32_t acr;
     uint32_t wait_state_status;
-    const uint32_t wait_state = (STM32F4_FLASH_W8_STATE << FLASH_ACR_LATENCY_SHIFT) & FLASH_ACR_LATENCY_MASK;
+    stm32_fmc_desc_t const * fmc_desc = stm32_fmc_get_desc(0); /* TODO: only flash0 supported by now */
+    const uint32_t wait_state = (fmc_desc->wait_state << FLASH_ACR_LATENCY_SHIFT) & FLASH_ACR_LATENCY_MASK;
     uint32_t count = 0UL;
     kstatus_t status = K_STATUS_OKAY;
+
+    if (unlikely((status = flash_map()) == K_STATUS_OKAY)) {
+        goto err;
+    }
 
     acr = ioread32(FLASH_BASE_ADDR + FLASH_ACR_REG);
     acr &= ~(FLASH_ACR_LATENCY_MASK);
@@ -42,8 +54,7 @@ kstatus_t flash_probe(void)
     if (unlikely(wait_state != wait_state_status)) {
         status = K_ERROR_NOTREADY;
     }
-
+    flash_unmap();
+err:
     return status;
 }
-
-{%- endif %}
