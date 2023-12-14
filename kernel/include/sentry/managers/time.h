78de245853183a96c7522516b28a7a12ab020d29
pthierry <philippe.thierry@ledger.fr>
2023-12-14 16:04:26 +0100

    time: adding get_cycle/micro/mili API

--- kernel/include/sentry/managers/time.h
+++ kernel/include/sentry/managers/time.h
@@ -9,6 +9,7 @@ extern "C" {
 #endif
 
 #include <sentry/arch/asm-generic/tick.h>
+#include <bsp/drivers/clk/rcc.h>
 #include <sentry/managers/task.h>
 #include <uapi/handle.h>
 
@@ -30,13 +31,34 @@ kstatus_t mgr_time_delay_add_job(taskh_t job, uint32_t delay_ms);
 kstatus_t mgr_time_delay_add_signal(taskh_t job, uint32_t delay_ms, sigh_t sig);
 
 
-static inline uint64_t mgr_time_ge_cycle(void) {
+static inline uint64_t mgr_time_get_cycle(void) {
     uint64_t ts = systime_get_cycleh();
     ts <<= 32;
     ts |= systime_get_cyclel();
     return ts;
 }
 
+static inline uint64_t mgr_time_get_micro(void) {
+    uint64_t ts = systime_get_cycleh();
+    uint32_t freq = rcc_get_core_frequency();
+    /* divide freq by number of micros in 1 sec */
+    freq /= 1000000UL;
+    ts <<= 32;
+    ts |= systime_get_cyclel();
+    ts /= freq;
+    return ts;
+}
+
+static inline uint64_t mgr_time_get_milli(void) {
+    uint64_t ts = systime_get_cycleh();
+    uint32_t freq = rcc_get_core_frequency();
+    /* divide freq by number of millis in 1 sec */
+    freq /= 1000UL;
+    ts <<= 32;
+    ts |= systime_get_cyclel();
+    ts /= freq;
+    return ts;
+}
 /**
  * delay ticker, to be called by the systick using JIFFIES_TO_MSEC(1)
  * to calculate back the ticker period
