d74c891a8a8f3f7b10ad1332b179fb3ce85e32a3
yhql <victor_servant@hotmail.com>
2024-01-12 15:07:09 +0100

    copy_user: error on buffer overlap

--- uapi/src/svc_exchange.rs
+++ uapi/src/svc_exchange.rs
@@ -1,3 +1,5 @@
+use systypes::Status;
+
 pub const SVC_EXCH_AREA_LEN: usize = 128; // TODO: replace by CONFIG-defined value
 
 /// This SVC exchange area is always defined for all apps, so it is declared
@@ -6,6 +8,31 @@ pub const SVC_EXCH_AREA_LEN: usize = 128; // TODO: replace by CONFIG-defined val
 #[link_section = ".svcexchange"]
 pub static mut SVC_EXCHANGE_AREA: [u8; SVC_EXCH_AREA_LEN] = [0u8; SVC_EXCH_AREA_LEN];
 
+unsafe fn check_bounds(pointer: *const u8, length: usize) -> Result<(), ()> {
+    let svcexc = SVC_EXCHANGE_AREA.as_ptr();
+    let svcexc_end = svcexc.add(SVC_EXCH_AREA_LEN);
+
+    // buffer starts in the middle of the exchange area, abort
+    if pointer >= svcexc && pointer <= svcexc_end {
+        return Err(());
+    }
+
+    // buffer ends in the exchange area, abort
+    // Note: this is unlikely to happen if `svc_exchange` is always assumed to be at
+    // the beginning of RAM
+    let buffer_end = pointer.add(length);
+    if buffer_end >= svcexc && buffer_end <= svcexc_end {
+        return Err(());
+    }
+
+    // exchange area is contained within the buffer, abort
+    if pointer <= svcexc && buffer_end >= svcexc_end {
+        return Err(());
+    }
+
+    Ok(())
+}
+
 /// Copy data to svc_exchange area
 ///
 /// # Safety
@@ -13,12 +40,16 @@ pub static mut SVC_EXCHANGE_AREA: [u8; SVC_EXCH_AREA_LEN] = [0u8; SVC_EXCH_AREA_
 /// Callers must ensure memory pointed to by `from` up to `from + length` belongs to
 /// a valid variable.
 #[no_mangle]
-pub unsafe extern "C" fn copy_from_user(from: *const u8, length: usize) {
-    core::ptr::copy(
+pub unsafe extern "C" fn copy_from_user(from: *const u8, length: usize) -> Status {
+    if check_bounds(from, length).is_err() {
+        return Status::Invalid;
+    }
+    core::ptr::copy_nonoverlapping(
         from,
         SVC_EXCHANGE_AREA.as_mut_ptr(),
         length.min(SVC_EXCH_AREA_LEN),
-    )
+    );
+    Status::Ok
 }
 
 /// Copy data from svc_exchange area to user-controlled buffer
@@ -28,12 +59,16 @@ pub unsafe extern "C" fn copy_from_user(from: *const u8, length: usize) {
 /// Callers must ensure memory pointed to by `from` up to `from + length` belongs to
 /// a valid variable.
 #[no_mangle]
-pub unsafe extern "C" fn copy_to_user(to: *mut u8, length: usize) {
-    core::ptr::copy(
+pub unsafe extern "C" fn copy_to_user(to: *mut u8, length: usize) -> Status {
+    if check_bounds(to, length).is_err() {
+        return Status::Invalid;
+    }
+    core::ptr::copy_nonoverlapping(
         SVC_EXCHANGE_AREA.as_ptr(),
         to,
         length.min(SVC_EXCH_AREA_LEN),
-    )
+    );
+    Status::Ok
 }
 
 #[cfg(test)]
@@ -60,5 +95,27 @@ mod tests {
             copy_to_user(res.as_mut_ptr(), string.len());
         }
         assert_eq!(&res[..], &string[..128]);
+    }
+
+    #[test]
+    fn invalid_src() {
+        unsafe {
+            assert_eq!(
+                copy_from_user(SVC_EXCHANGE_AREA.as_ptr(), 4),
+                Status::Invalid
+            );
+        }
+    }
+
+    #[test]
+    fn invalid_dest() {
+        let string = [b'z'; 4];
+        unsafe {
+            copy_from_user(string.as_ptr(), string.len());
+            assert_eq!(
+                copy_to_user(SVC_EXCHANGE_AREA.as_mut_ptr(), string.len()),
+                Status::Invalid
+            );
+        }
     }
 }
