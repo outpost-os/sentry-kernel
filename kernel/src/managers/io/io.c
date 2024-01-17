e478a2498b8bda6ab81b6550a9f69cb292a7aee7
pthierry <philippe.thierry@ledger.fr>
2024-01-17 16:56:59 +0100

    fixing io manager API, using port/pin as unique identifier tuple

--- kernel/src/managers/io/io.c
+++ kernel/src/managers/io/io.c
@@ -12,6 +12,7 @@
 
 /* BSP headers */
 #include <bsp/drivers/gpio/gpio.h>
+#include <bsp/drivers/gpio/pinctrl.h>
 #ifdef CONFIG_SOC_FAMILY_STM32
 #include <bsp/drivers/exti/exti.h>
 #endif
@@ -67,10 +68,10 @@ kstatus_t mgr_io_autotest(void)
  *
  * This set output moode I/O line value to high level
  */
-kstatus_t mgr_io_set(ioh_t ioh)
+kstatus_t mgr_io_set(uint8_t ioport, uint8_t iopin)
 {
     kstatus_t status = K_STATUS_OKAY;
-    status = gpio_set(ioh.ioport, ioh.iopin);
+    status = gpio_set(ioport, iopin);
     return status;
 }
 
@@ -79,10 +80,10 @@ kstatus_t mgr_io_set(ioh_t ioh)
  *
  * This set output moode I/O line value to low level
  */
-kstatus_t mgr_io_reset(ioh_t ioh)
+kstatus_t mgr_io_reset(uint8_t ioport, uint8_t iopin)
 {
     kstatus_t status = K_STATUS_OKAY;
-    status = gpio_reset(ioh.ioport, ioh.iopin);
+    status = gpio_reset(ioport, iopin);
     return status;
 }
 
@@ -94,84 +95,93 @@ kstatus_t mgr_io_reset(ioh_t ioh)
 /*@
   @ requires \valid(val);
   */
-kstatus_t mgr_io_read(ioh_t ioh, bool *val)
+kstatus_t mgr_io_read(uint8_t ioport, uint8_t iopin, bool *val)
 {
     kstatus_t status = K_STATUS_OKAY;
-    status = gpio_get(ioh.ioport, ioh.iopin, val);
+    status = gpio_get(ioport, iopin, val);
     return status;
 }
 
 /**
- * @brief Configure I/O identified by ioh
+ * @brief Configure I/O identified by io_info_t
  *
- * TODO: define the I/O configuration structure that is shared between user & kernel space
- *  (uapi definition)
+ * io_info_t is a part of a devinfo structure (see uapi/device API)
  */
-kstatus_t mgr_io_configure(ioh_t ioh)
+kstatus_t mgr_io_configure(io_info_t io)
 {
     kstatus_t status = K_STATUS_OKAY;
+    gpio_pinctrl_desc_t pinctrl_desc = {
+        .mode = io.mode,
+        .altfunc = io.af,
+        .type = io.pupdr,
+        .speed = io.speed,
+        .pull_mode = io.ppull,
+        .port_id = io.pin,
+        .pin = io.pin
+    };
+    status = gpio_pinctrl_configure(pinctrl_desc);
     return status;
 }
 
 /* About interrupt associated to I/O */
 
 
-kstatus_t mgr_io_mask_interrupt(ioh_t ioh)
+kstatus_t mgr_io_mask_interrupt(uint8_t ioport, uint8_t iopin)
 {
     kstatus_t status = K_STATUS_OKAY;
 #ifdef CONFIG_SOC_SUBFAMILY_STM32F4
     /* On stm32f4, IT0 == Px0, IT1 == Px1, and so on...
      * meaning that io pin (upto 15) define the associated EXTI line
      */
-    status = exti_mask_interrupt(ioh.iopin);
+    status = exti_mask_interrupt(iopin);
 #endif
     return status;
 }
 
-kstatus_t mgr_io_unmask_interrupt(ioh_t ioh)
+kstatus_t mgr_io_unmask_interrupt(uint8_t ioport, uint8_t iopin)
 {
     kstatus_t status = K_STATUS_OKAY;
 #ifdef CONFIG_SOC_SUBFAMILY_STM32F4
     /* On stm32f4, IT0 == Px0, IT1 == Px1, and so on...
      * meaning that io pin (upto 15) define the associated EXTI line
      */
-    status = exti_unmask_interrupt(ioh.iopin);
+    status = exti_unmask_interrupt(iopin);
 #endif
     return status;
 }
 
-kstatus_t mgr_io_mask_event(ioh_t ioh)
+kstatus_t mgr_io_mask_event(uint8_t ioport, uint8_t iopin)
 {
     kstatus_t status = K_STATUS_OKAY;
 #ifdef CONFIG_SOC_SUBFAMILY_STM32F4
     /* On stm32f4, IT0 == Px0, IT1 == Px1, and so on...
      * meaning that io pin (upto 15) define the associated EXTI line
      */
-    status = exti_mask_event(ioh.iopin);
+    status = exti_mask_event(iopin);
 #endif
     return status;
 }
 
-kstatus_t mgr_io_unmask_event(ioh_t ioh)
+kstatus_t mgr_io_unmask_event(uint8_t ioport, uint8_t iopin)
 {
     kstatus_t status = K_STATUS_OKAY;
 #ifdef CONFIG_SOC_SUBFAMILY_STM32F4
     /* On stm32f4, IT0 == Px0, IT1 == Px1, and so on...
      * meaning that io pin (upto 15) define the associated EXTI line
      */
-    status = exti_unmask_event(ioh.iopin);
+    status = exti_unmask_event(iopin);
 #endif
     return status;
 }
 
-kstatus_t mgr_io_clear_pending_interrupt(ioh_t ioh)
+kstatus_t mgr_io_clear_pending_interrupt(uint8_t ioport, uint8_t iopin)
 {
     kstatus_t status = K_STATUS_OKAY;
 #ifdef CONFIG_SOC_SUBFAMILY_STM32F4
     /* On stm32f4, IT0 == Px0, IT1 == Px1, and so on...
      * meaning that io pin (upto 15) define the associated EXTI line
      */
-    status = exti_clear_pending(ioh.iopin);
+    status = exti_clear_pending(iopin);
 #endif
     return status;
 }
