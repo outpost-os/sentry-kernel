f0d0724ee0996ee35156914522a4c4467c5595b8
pthierry <philippe.thierry@ledger.fr>
2024-02-27 11:41:53 +0100

    autotest: adding gethandle test suite

---
+++ autotest/src/tests/test_handle.c
@@ -0,0 +1,42 @@
+#include <inttypes.h>
+#include <testlib/log.h>
+#include <testlib/assert.h>
+#include <uapi/uapi.h>
+#include "test_ipc.h"
+
+typedef struct __attribute__((packed)) ktaskh  {
+    uint32_t rerun : 20;
+    uint32_t id : 8;
+    uint32_t family : 3;
+} ktaskh_t;
+
+void test_gethandle(void)
+{
+    Status ret;
+    taskh_t handle = 0;
+    ktaskh_t khandle;
+
+    TEST_START();
+    /* clear svcexchange first */
+    copy_from_user((uint8_t*)&handle, sizeof(handle));
+    copy_to_user((uint8_t*)&handle, sizeof(handle));
+    ASSERT_EQ(handle, 0);
+    ret = sys_get_process_handle(0xbabeUL);
+    copy_to_user((uint8_t*)&handle, sizeof(taskh_t));
+    ASSERT_EQ(ret, STATUS_OK);
+    LOG("received handle: %lx", handle);
+    __builtin_memcpy(&khandle, &handle, sizeof(ktaskh_t));
+    LOG("handle rerun = %lx", (uint32_t)khandle.rerun);
+    LOG("handle id = %lx", (uint32_t)khandle.id);
+    LOG("handle family = %lx", (uint32_t)khandle.family);
+    TEST_END();
+}
+
+void test_handle(void)
+{
+    TEST_SUITE_START("sys_get_handle");
+
+    test_gethandle();
+
+    TEST_SUITE_END("sys_get_handle");
+}
---
+++ autotest/src/tests/test_handle.h
@@ -0,0 +1,6 @@
+#ifndef TEST_HANDLE_H
+#define TEST_HANDLE_H
+
+void test_handle(void);
+
+#endif/*!TEST_HANDLE_H*/
