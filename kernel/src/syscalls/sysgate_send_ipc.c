ac0ecdaf098c3f3cbc9886cf585d88dde5a49268
pthierry <philippe.thierry@ledger.fr>
2024-03-04 10:03:00 +0100

    fix: signal and ipc gate waitforevent state check

--- kernel/src/syscalls/sysgate_send_ipc.c
+++ kernel/src/syscalls/sysgate_send_ipc.c
@@ -36,18 +36,13 @@ stack_frame_t *gate_send_ipc(stack_frame_t *frame, taskh_t target, uint32_t len)
     mgr_task_set_sysreturn(current, STATUS_NON_SENSE);
     /* check if target can be sheduled again */
     mgr_task_get_state(target, &dest_state);
-    if ((dest_state != JOB_STATE_IPC_SEND_BLOCKED) &&
-        (dest_state != JOB_STATE_SLEEPING_DEEP)) {
-        /* awake target job only if not already waiting for another IPC and if
-         * not in deep sleep
-         */
-        if (dest_state == JOB_STATE_SLEEPING) {
-            /* if target job was sleeping, set return to ok */
-            /* FIXME: define a dedicated return code */
-            mgr_task_set_sysreturn(target, STATUS_OK);
-            mgr_task_set_state(target, JOB_STATE_READY);
-            sched_schedule(target);
-        }
+    if ((dest_state == JOB_STATE_SLEEPING) ||
+        (dest_state == JOB_STATE_WAITFOREVENT)) {
+        /* if target job was sleeping, set return to ok */
+        /* FIXME: define a dedicated return code */
+        mgr_task_set_sysreturn(target, STATUS_OK);
+        mgr_task_set_state(target, JOB_STATE_READY);
+        sched_schedule(target);
     }
     next = sched_elect();
     if (unlikely(mgr_task_get_sp(next, &next_frame) != K_STATUS_OKAY)) {
