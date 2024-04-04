aeadb94f3524514e61a4e6263c20f5828c224745
pthierry <philippe.thierry@ledger.fr>
2024-04-04 09:17:27 +0200

    fix: updated autotest to use new UAPI type

--- autotest/src/tests/test_signal.c
+++ autotest/src/tests/test_signal.c
@@ -2,6 +2,7 @@
 #include <testlib/log.h>
 #include <testlib/assert.h>
 #include <uapi/uapi.h>
+#include <uapi/types.h>
 #include "test_ipc.h"
 
 
@@ -11,6 +12,8 @@ void test_signal_sendrecv(void)
     taskh_t handle = 0;
     uint32_t timeout = 20UL;
     uint8_t data[CONFIG_SVC_EXCHANGE_AREA_LEN] = {0};
+    exchange_event_t *header;
+
     ret = sys_get_process_handle(0xbabeUL);
     copy_to_user((uint8_t*)&handle, sizeof(taskh_t));
     LOG("handle is %lx", handle);
@@ -21,10 +24,16 @@ void test_signal_sendrecv(void)
         LOG("sending signal %u to myself", sig);
         ret = sys_send_signal(handle, sig);
         ret = sys_wait_for_event(EVENT_TYPE_SIGNAL, timeout);
-        copy_to_user(data, 12);
-        taskh_t *source = (taskh_t*)&data[4];
-        uint32_t *signal = (uint32_t*)&data[8];
-        LOG("%x:%u:%x%x: source=%lx, signal=%lu", data[0], data[1], data[2], data[3], *source, *signal);
+        copy_to_user(data, 4+sizeof(exchange_event_t));
+        header = (exchange_event_t*)&data[0];
+        uint32_t* content = (char*)&header->data[0];
+        LOG("%x:%u:%x:src=%lx signal=%lu",
+            header->type,
+            header->length,
+            header->magic,
+            header->source,
+            content);
+
         ASSERT_EQ(ret, STATUS_OK);
     }
     TEST_END();
