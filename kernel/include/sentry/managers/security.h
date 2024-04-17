811cde95af245629eb61baa4dd796503118c9417
Florent Valette <florent.valette@ledger.fr>
2024-04-17 13:49:44 +0200

    managers,task: generate task meta layout from json description
    Task meta structure layout might be used by different programming languages
    in a native representation, we need a single root of trust for memory
    layout. To do so, per languages, dedicated type are generated from a json file
    and using jinja template processing.

--- kernel/include/sentry/managers/security.h
+++ kernel/include/sentry/managers/security.h
@@ -4,34 +4,11 @@
 #include <uapi/uapi.h>
 #include <sentry/ktypes.h>
 
+#include <sentry/managers/capability.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-/**
- * @file Sentry I/O manager API
- */
-
-typedef enum capability {
-    CAP_DEV_BUSES        = (0x1UL  << 0),
-    CAP_DEV_IO           = (0x1UL  << 1),
-    CAP_DEV_DMA          = (0x1UL  << 2),
-    CAP_DEV_ANALOG       = (0x1UL  << 3),
-    CAP_DEV_TIMER        = (0x1UL  << 4),
-    CAP_DEV_STORAGE      = (0x1UL  << 5),
-    CAP_DEV_CRYPTO       = (0x1UL  << 6),
-    CAP_DEV_CLOCK        = (0x1UL  << 7),
-    CAP_DEV_POWER        = (0x1UL  << 8),
-    CAP_DEV_NEURAL       = (0x1UL  << 9),
-    CAP_SYS_UPGRADE      = (0x1UL  << 12),
-    CAP_SYS_POWER        = (0x1UL  << 13),
-    CAP_SYS_PROCSTART    = (0x1UL  << 14),
-    CAP_MEM_SHM_OWN      = (0x1UL  << 16),
-    CAP_MEM_SHM_USE      = (0x1UL  << 17),
-    CAP_MEM_SHM_TRAHSFER = (0x1UL  << 18),
-    CAP_TIM_HP_CHRONO    = (0x1UL  << 20),
-    CAP_CRY_KRNG         = (0x1UL  << 21),
-} capability_t;
 
 /** @def mask for dev capability. dev capa hold 11 first bytes of register */
 #define CAP_DEV_MASK ((0x1UL  << 12) - 1)
