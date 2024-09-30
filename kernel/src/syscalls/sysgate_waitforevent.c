3e9e7a8ae9e9d72c5eedb7b5741ccab82db23a17
pthierry <philippe.thierry@ledger.fr>
2024-09-30 15:41:22 +0200

    gate: cleaning wait for event hierarchy

--- kernel/src/syscalls/sysgate_waitforevent.c
+++ kernel/src/syscalls/sysgate_waitforevent.c
@@ -8,6 +8,54 @@
 #include <uapi/types.h>
 
 
+static inline void gate_waitforevent_populate_dma(taskh_t current, dmah_t dma, dma_chan_state_t event)
+{
+    task_meta_t const *meta;
+    uint8_t *svc;
+    exchange_event_t *dest_svcexch;
+    /* forge SVC exchange with received signal informations */
+    if (unlikely(mgr_task_get_metadata(current, &meta) != K_STATUS_OKAY)) {
+        /* this should never happen !*/
+        /*@ assert \false; */
+        panic(PANIC_KERNEL_INVALID_MANAGER_RESPONSE);
+    }
+    svc = task_get_svcexchange(meta);
+    dest_svcexch = (exchange_event_t*)svc;
+    /* set T,L values from TLV */
+    dest_svcexch->type = EVENT_TYPE_DMA;
+    dest_svcexch->length = sizeof(uint8_t);
+    dest_svcexch->magic = 0x4242; /** FIXME: define a magic shared with uapi */
+    dest_svcexch->source = dma; /**< stream source of event */
+    /* here, event is encoded on 8bits length */
+    dest_svcexch->data[0] = event;
+}
+
+/**
+ * @fn gate_waitforevent_populate_signal : populate svc exchange with signal info
+ */
+static inline void gate_waitforevent_populate_signal(taskh_t current, uint8_t sig, taskh_t source)
+{
+    task_meta_t const *meta;
+    uint8_t *svc;
+    exchange_event_t *dest_svcexch;
+    /* forge SVC exchange with received signal informations */
+    if (unlikely(mgr_task_get_metadata(current, &meta) != K_STATUS_OKAY)) {
+        /* this should never happen !*/
+        /*@ assert \false; */
+        panic(PANIC_KERNEL_INVALID_MANAGER_RESPONSE);
+    }
+    svc = task_get_svcexchange(meta);
+    dest_svcexch = (exchange_event_t*)svc;
+    /* set T,L values from TLV */
+    dest_svcexch->type = EVENT_TYPE_SIGNAL;
+    dest_svcexch->length = sizeof(uint32_t);
+    dest_svcexch->magic = 0x4242; /** FIXME: define a magic shared with uapi */
+    dest_svcexch->source = source;
+    uint32_t *sigdata = (uint32_t*)&dest_svcexch->data;
+    sigdata[0] = sig;
+
+}
+
 stack_frame_t *gate_waitforevent(stack_frame_t *frame,
                                uint8_t          mask,
                                int32_t          timeout)
@@ -21,22 +69,7 @@ stack_frame_t *gate_waitforevent(stack_frame_t *frame,
         uint32_t sig;
         taskh_t source;
         if (mgr_task_load_sig_event(current, &sig, &source) == K_STATUS_OKAY) {
-            task_meta_t const *meta;
-            uint8_t *svc;
-            exchange_event_t *dest_svcexch;
-
-            /* forge SVC exchange with received signal informations */
-            mgr_task_get_metadata(current, &meta);
-            svc = task_get_svcexchange(meta);
-            dest_svcexch = (exchange_event_t*)svc;
-            /* set T,L values from TLV */
-            dest_svcexch->type = EVENT_TYPE_SIGNAL;
-            dest_svcexch->length = sizeof(uint32_t);
-            dest_svcexch->magic = 0x4242; /** FIXME: define a magic shared with uapi */
-            dest_svcexch->source = source;
-            uint32_t *sigdata = (uint32_t*)&dest_svcexch->data;
-            sigdata[0] = sig;
-
+            gate_waitforevent_populate_signal(current, sig, source),
             mgr_task_set_sysreturn(current, STATUS_OK);
             goto end;
         }
@@ -55,21 +88,7 @@ stack_frame_t *gate_waitforevent(stack_frame_t *frame,
         dmah_t dmah;
         dma_chan_state_t event;
         if (mgr_task_load_dma_event(current, &dmah, &event) == K_STATUS_OKAY) {
-            task_meta_t const *meta;
-            uint8_t *svc;
-            exchange_event_t *dest_svcexch;
-
-            /* forge SVC exchange with received signal informations */
-            mgr_task_get_metadata(current, &meta);
-            svc = task_get_svcexchange(meta);
-            dest_svcexch = (exchange_event_t*)svc;
-            /* set T,L values from TLV */
-            dest_svcexch->type = EVENT_TYPE_DMA;
-            dest_svcexch->length = sizeof(uint32_t);
-            dest_svcexch->magic = 0x4242; /** FIXME: define a magic shared with uapi */
-            dest_svcexch->source = dmah; /**< stream source of event */
-            /* here, event is encoded on 8bits length */
-            dest_svcexch->data[0] = event;
+            gate_waitforevent_populate_dma(current, dmah, event);
             mgr_task_set_sysreturn(current, STATUS_OK);
             goto end;
         }
