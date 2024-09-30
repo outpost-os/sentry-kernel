68c8271d032212b12192703e7c6d0d7d2598a954
Florent Valette <florent.valette@ledger.fr>
2024-09-30 14:05:41 +0200

    support/meson/version: fix case where there is no tags
    This happens with personal fork(s).

--- support/meson/version.sh
+++ support/meson/version.sh
@@ -3,11 +3,10 @@
 # SPDX-FileCopyrightText: 2023 Ledger SAS
 # SPDX-License-Identifier: Apache-2.0
 
-
 if [ "$1" = "get-vcs" ]; then
-  git -C "$MESON_SOURCE_ROOT" describe --tags --dirty
+    git -C "$MESON_SOURCE_ROOT" describe --tags --dirty --always
 elif [ "$1" = "set-dist" ]; then
-  $MESONREWRITE --sourcedir="$MESON_PROJECT_DIST_ROOT" kwargs set project / version "$2"
+    $MESONREWRITE --sourcedir="$MESON_PROJECT_DIST_ROOT" kwargs set project / version "$2"
 else
-  exit 1
+    exit 1
 fi
